cd7aa90705a2e73daacb6e476a0406c187f7615d
Preston Hom <Preston@prestons-air.lan>
2019-10-06 15:58:41 -0700

    adds token auth

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import App from './pages/App/App';
+// import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <App />,
+  <Router><Route component={App}/></Router>,
   document.getElementById('root')
 );
