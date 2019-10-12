06895540428c78b70b27a0b3f82214d2b3d4a6ed
Preston Hom <Preston@Prestons-MacBook-Air.local>
2019-10-12 02:10:00 -0700

    tests cors anywhere fix

--- src/services/restaurant-api.js
+++ src/services/restaurant-api.js
@@ -1,4 +1,4 @@
-const BASE_URL= 'http://cors-anywhere.herokuapp.com/https://menunav.herokuapp.com/api/v1/'
+const BASE_URL= 'https://cors-anywhere.herokuapp.com/https://menunav.herokuapp.com/api/v1/'
 
 export async function getRestaurants(){
     return fetch(BASE_URL, {mode: 'cors'})
